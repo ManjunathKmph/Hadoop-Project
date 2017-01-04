64bc72367b4f5914c58070c07d0f05b2057bf5a2
Manjunath Kempaiah <manjunath.kmph@gmail.com>
2017-01-04 20:23:20 +0530

    Updated

--- src/main/java/com/manju/hadoopproject/dnamirror/DNAMirrorReducer.java
+++ src/main/java/com/manju/hadoopproject/dnamirror/DNAMirrorReducer.java
@@ -1,6 +1,10 @@
 package com.manju.hadoopproject.dnamirror;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -9,8 +13,13 @@ public class DNAMirrorReducer  extends Reducer<DNAMirrorKey,Text,NullWritable,Te
 	
 	public void reduce(DNAMirrorKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 		StringBuilder sb = new StringBuilder();
+		List<String> valuesList = new ArrayList<String>();
 		for (Text val : values) {
-			sb.append(val.toString() + ",");
+			valuesList.add(val.toString());
+		}
+		Collections.sort(valuesList);
+		for(String val : valuesList) {
+			sb.append(val + ",");
 		}
 		context.write(NullWritable.get(), new Text(sb.toString().substring(0, sb.length()-1)));
 	}
